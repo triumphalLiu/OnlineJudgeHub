9bc05854d9747dc5732584cdc0e16ca3773069ce
liu <liu@triumphal.cn>
2018-05-27 14:18:16 +0800

    Finish Question 3

--- OJ/Solution.cpp
+++ OJ/Solution.cpp
@@ -51,4 +51,33 @@ public:
 			}
 		}
 	}
+
+	/*
+	*题目描述
+	*	输入一个链表，从尾到头打印链表每个节点的值。
+	*   struct ListNode {
+	*         int val;
+	*         struct ListNode *next;
+	*         ListNode(int x) :
+	*               val(x), next(NULL) {
+	*         }
+	*   };
+	*	时间限制：1秒 空间限制：32768K
+	*思路描述
+	*	使用栈的数据结构，按顺序遍历链表并压栈，然后一个一个出栈放入vector中。
+	*/
+	vector<int> printListFromTailToHead(ListNode* head) {
+		vector<int> result;
+		stack<int> middle;
+		ListNode *pt = head;
+		while (pt != NULL) {
+			middle.push(pt->val);
+			pt = pt->next;
+		}
+		while (middle.empty() == false) {
+			result.push_back(middle.top());
+			middle.pop();
+		}
+		return result;
+	}
 };
--- OJ/global.h
+++ OJ/global.h
@@ -4,5 +4,14 @@
 #include <cstdio>
 #include <cstring>
 #include <vector>
+#include <stack>
 #include <iostream>
 using namespace std;
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+		val(x), next(NULL) {
+	}
+};
